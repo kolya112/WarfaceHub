b9b1c57ebaf48d5e6066cb798914816c4b423331
kolya112 <nikolayurch@gmail.com>
2021-12-19 20:16:51 +0300

    1.0

--- WarfaceHub/Form1.Designer.cs
+++ WarfaceHub/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.warning = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.stats = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.stats);
+            this.panel1.Controls.Add(this.warning);
+            this.panel1.Location = new System.Drawing.Point(12, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 409);
+            this.panel1.TabIndex = 0;
+            // 
+            // warning
+            // 
+            this.warning.AutoSize = true;
+            this.warning.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.warning.Location = new System.Drawing.Point(18, 161);
+            this.warning.Name = "warning";
+            this.warning.Size = new System.Drawing.Size(246, 26);
+            this.warning.TabIndex = 0;
+            this.warning.Text = "Для отображения статистики персонажа\r\nвведите его ник в поле справа";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Статистика персонажа";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(310, 48);
+            this.textBox1.MaxLength = 16;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Показать статистику";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // stats
+            // 
+            this.stats.AutoSize = true;
+            this.stats.Location = new System.Drawing.Point(21, 19);
+            this.stats.Name = "stats";
+            this.stats.Size = new System.Drawing.Size(25, 13);
+            this.stats.TabIndex = 1;
+            this.stats.Text = "Info";
+            this.stats.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WarfaceHub";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label warning;
+        private System.Windows.Forms.Label stats;
     }
 }
 
--- WarfaceHub/Form1.cs
+++ WarfaceHub/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 namespace WarfaceHub
 {
@@ -15,6 +18,88 @@ namespace WarfaceHub
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0)
+            {
+                MessageBox.Show("Введите ник в поле ввода", "Ошибка");
+            }
+            else
+            {
+                string nickname = textBox1.Text;
+                warning.Visible = false;
+                stats.Visible = true;
+                    HttpWebResponse response = (HttpWebResponse)WebRequest.Create($"http://api.warface.ru/user/stat/?name={nickname}&server=0").GetResponse();
+                    JSON playerinfo = JsonConvert.DeserializeObject<JSON>(new StreamReader(response.GetResponseStream()).ReadToEnd());
+                    response.Close();
+
+                    string result = @$" 
+                    Ник: {playerinfo.nickname},
+                    ID: {playerinfo.user_id},
+                    Клан: {playerinfo.clan_name},
+                    ID клана: {playerinfo.clan_id},
+                    Ранг:  {playerinfo.rank_id},
+                    Опыт: {playerinfo.experience},
+                    Времени в игре: {playerinfo.playtime_h} часов, {playerinfo.playtime_m} минут,
+                    PVP:
+                    У/С: {playerinfo.pvp},
+                    Убийств в общем: {playerinfo.kill},
+                    Убийств врагов: {playerinfo.kills},
+                    Убийств тиммейтов: {playerinfo.friendly_kills},
+                    Смертей: {playerinfo.death},
+                    Общее количество ссыграных игр: {playerinfo.pvp_all},
+                    Выйграно игр: {playerinfo.pvp_wins},
+                    Проиграно игр: {playerinfo.pvp_lost},
+                    Любимый класс: {playerinfo.favoritPVP}, 
+                    PVE:
+                    У/С: {playerinfo.pve},
+                    Убийств в общем: {playerinfo.pve_kill},
+                    Убийств врагов: {playerinfo.pve_kills},
+                    Убийств тиммейтов: {playerinfo.pve_friendly_kills},
+                    Смертей: {playerinfo.pve_death},
+                    Общее количество ссыграных игр: {playerinfo.pve_all},
+                    Выйграно игр: {playerinfo.pve_wins},
+                    Проиграно игр: {playerinfo.pve_lost},
+                    Любимый класс: {playerinfo.favoritPVE},";
+
+                stats.Text = result;
+            }
+        }
+
+        class JSON
+        {
+            public string user_id { get; set; }
+            public string nickname { get; set; }
+            public int experience { get; set; }
+            public int rank_id { get; set; }
+            public bool is_transparent { get; set; }
+            public int clan_id { get; set; }
+            public string clan_name { get; set; }
+            public int kill { get; set; }
+            public int friendly_kills { get; set; }
+            public int kills { get; set; }
+            public int death { get; set; }
+            public double pvp { get; set; }
+            public int pve_kill { get; set; }
+            public int pve_friendly_kills { get; set; }
+            public int pve_kills { get; set; }
+            public int pve_death { get; set; }
+            public double pve { get; set; }
+            public int playtime { get; set; }
+            public int playtime_h { get; set; }
+            public int playtime_m { get; set; }
+            public string favoritPVP { get; set; }
+            public string favoritPVE { get; set; }
+            public int pve_wins { get; set; }
+            public int pvp_wins { get; set; }
+            public int pvp_lost { get; set; }
+            public int pve_lost { get; set; }
+            public int pve_all { get; set; }
+            public int pvp_all { get; set; }
+            public float pvpwl { get; set; }
+            public string full_response { get; set; }
         }
     }
 }
